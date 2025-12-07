cc64fdfa0242726250acbeda7d01463a797b887b
Your Name <youremail@example.com>
2025-12-07 20:08:35 +0300

    Frontend: Event Feed components, filters, and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from "react" 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  
   return (
-    <>
-    <h1 className="text-blue-500">eventease</h1>
-    </>
-  )
+    <BrowserRouter basename="/frontend-eventease/">  {/* <-- add this */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/event/:id" element={<div>Event Details Page</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
